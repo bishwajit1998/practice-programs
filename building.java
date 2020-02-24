724a93d088795a9754f93614e57ffa065f2d428c
bishwajit <38694064+bishwajit1998@users.noreply.github.com>
2020-02-24 19:52:12 +0530

    Add files via upload

---
+++ building.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.Scanner;
+import java.lang.*;
+class building
+{
+ //main method
+   public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        //total no of people
+        int n = s.nextInt();
+        int id[]=new int[n+1];
+        //setting id's
+        for(int i=1;i<n+1;i++)
+        {
+           id[i]=i;
+           //System.out.println(i);
+        }
+        //minimum no. of people in each building
+        int min = s.nextInt();
+        int x[]=new int[n+1];
+        int y[]=new int[y+1];
+        //setting up co-ordinates for each person
+        for(int i=1;i<n+1;i++)
+        {
+           x[i]=s.nextInt();
+           y[i]=s.nextInt();
+        }
+        int dist;
+        dist
+      
+    }
+}
